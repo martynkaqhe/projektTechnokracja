64bce7f92969b6012bc8e69fc0475c0cb0c58d1e
Daniel <63232808+Twojstary12@users.noreply.github.com>
2022-05-31 20:21:30 +0200

    Update logout.php

--- PHP/logout.php
+++ PHP/logout.php
@@ -1,8 +1,8 @@
 <?php
     session_start();
-    // Destroy session
+    // Kasuje sesję
     if(session_destroy()) {
-        // Redirecting To Home Page
+        // Przerzuca do loginu
         header("Location: login.php");
     }
 ?>
